47ed9f065651df4afdbfc99d42fa85e487439c55
Jireh Lau <jirehcylau@gmail.com>
2021-03-30 01:28:34 -0400

    half way done

---
+++ finchcollector/main_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('restaurants/', views.restaurants_index, name='index'),
+    path('restaurants/<int:r_id>/', views.restaurants_detail, name='detail'),
+    path('restaurants/create/', views.RestaurantCreate.as_view(), name='restaurants_create'),
+    path('restaurants/<int:pk>/update/', views.RestaurantUpdate.as_view(), name='restaurants_update'),
+    path('restaurants/<int:pk>/delete/', views.RestaurantDelete.as_view(), name='restaurants_delete'),
+]
+
